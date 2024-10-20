e8dfa8bfe341ac12f7b7902e19c001872e48a335
Apoorva Kumar .P <bpapoorva@gmail.com>
2024-10-20 13:12:28 +0530

    Writing all testcases in test file

--- src/__test__/StringCalculator.test.jsx
+++ src/__test__/StringCalculator.test.jsx
@@ -10,4 +10,28 @@ describe('String Calculator', () => {
     expect(add("1")).toBe(1);
   });
 
+  test('returns the sum of two comma-separated numbers', () => {
+    expect(add("1,5")).toBe(6);
+  });
+
+  test('handles multiple numbers', () => {
+    expect(add("1,2,3,4")).toBe(10);
+  });
+
+  test('handles new lines as separators', () => {
+    expect(add("1\n2,3")).toBe(6);
+  });
+
+  test('supports custom delimiters', () => {
+    expect(add("//;\n1;2;3")).toBe(6);
+  });
+
+  test('throws an error for negative numbers', () => {
+    expect(() => add("1,-2,3")).toThrow("negative numbers not allowed: -2");
+  });
+
+  test('throws an error with all negative numbers in the message', () => {
+    expect(() => add("-1,-2,3")).toThrow("negative numbers not allowed: -1,-2");
+  });
+
 })
