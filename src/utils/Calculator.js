34ed1b1ba02deede2a15e3ef59f6e2a387a1bcee
Apoorva Kumar .P <bpapoorva@gmail.com>
2024-10-28 17:29:02 +0530

    changed code according to Single responsibilty principle

--- src/utils/Calculator.js
+++ src/utils/Calculator.js
@@ -1,3 +1,5 @@
+let delimiter = ',';
+
 export function add(numbers) {
   if (numbers === '' || numbers === undefined) {
     return 0;
@@ -9,23 +11,17 @@ export function add(numbers) {
   };
 
   // Default delimiter
-  let delimiter = ',';
   /**  If number starts with '//' ,then we have custom delimiter other than ','
    *   Identify the delimiter by getting the string after // till new line(\n) , remaining string is the required number
    *   then split the number with delimiter or new line and add it.
   */
   if (numbers.startsWith('//')) {
-    const delimiterEndIndex = numbers.indexOf('\n');
-    delimiter = numbers.substring(2, delimiterEndIndex);
-    numbers = numbers.substring(delimiterEndIndex + 1);
+    numbers = handleCustomDelimiter(numbers);
   }
 
   const numberArray = numbers.split(new RegExp(`[${delimiter}\n]`));
 
-  const negatives = numberArray.filter(num => +num < 0);
-  if (negatives.length > 0) {
-    throw new Error(`negative numbers not allowed: ${negatives.join(',')}`);
-  }
+  checkForNegativeNumbers(numberArray);
 
   if (delimiter === '*') {
     return numberArray.reduce((sum, num) => sum * +num, 1);
@@ -33,3 +29,17 @@ export function add(numbers) {
     return numberArray.reduce((sum, num) => sum + +num, 0);
   }
 };
+
+function handleCustomDelimiter(numbers) {
+  const delimiterEndIndex = numbers.indexOf('\n');
+  delimiter = numbers.substring(2, delimiterEndIndex);
+  numbers = numbers.substring(delimiterEndIndex + 1);
+  return numbers;
+}
+
+function checkForNegativeNumbers(numberArray) {
+  const negatives = numberArray.filter(num => +num < 0);
+  if (negatives.length > 0) {
+    throw new Error(`negative numbers not allowed: ${negatives.join(',')}`);
+  }
+}
